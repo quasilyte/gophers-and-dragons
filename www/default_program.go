1050b2124daee10eecc7fd3b7e10f1426890e73e
Iskander Sharipov <quasilyte@gmail.com>
2020-09-26 04:17:40 +0300

    www: accept game.State by value
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- www/default_program.go
+++ www/default_program.go
@@ -2,20 +2,20 @@ package tactic
 
 import "github.com/quasilyte/gophers-and-dragons/game"
 
-func ChooseCard(s *game.State) game.CardType {
+func ChooseCard(s game.State) game.CardType {
 	return tactic1(s)
 }
 
 // tactic1 is a trivial tactic that always retreats.
 // You can't win like this, but it's a minimal working
 // example that manages to pass the entire game without dying.
-func tactic1(s *game.State) game.CardType {
+func tactic1(s game.State) game.CardType {
 	return game.CardRetreat
 }
 
 // tactic2 will only fight with the easiest kind of
 // monsters and will route if wounded.
-func tactic2(s *game.State) game.CardType {
+func tactic2(s game.State) game.CardType {
 	if s.Avatar.HP < 10 {
 		return game.CardRetreat
 	}
